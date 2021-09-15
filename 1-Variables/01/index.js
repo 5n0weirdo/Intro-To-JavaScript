321b8bf30b03166a9e5a85cdaea7f48a17938508
5n0weirdo <snoweirdo@protonmail.com>
2021-09-15 14:22:04 +0200

    Variables

---
+++ 1-Variables/01/index.js
@@ -0,0 +1,8 @@
+var jsDev = "Dev";
+
+var fname = "rust"; 
+    console.log(fname);
+
+var age = 100;
+    console.log("you are" ,age, "years old");
+
---
+++ 1-Variables/02/index.js
@@ -0,0 +1,7 @@
+var age = 20;
+var totalAge = age % 11;
+    console.log(totalAge);
+
+var num = 15;
+var num = "fifteen";
+    console.log(num);
