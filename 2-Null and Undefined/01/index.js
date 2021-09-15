9b2640cfe29a627d96b90533d0f45902c1a1b56c
5n0weirdo <snoweirdo@protonmail.com>
2021-09-15 14:40:11 +0200

    Null and Undefined

---
+++ 2-Null and Undefined/01/index.js
@@ -0,0 +1,15 @@
+// Variable without a value will show undefined (no value)
+var name;
+console.log(name);
+
+// Assign value to a variable with no value
+name = "developer";
+console.log(name);
+
+// Assign value to variable
+var age = 200;
+console.log(age);
+
+// null is "explicitly nothing" (will print out 'null')
+var player = null;
+console.log(player);
