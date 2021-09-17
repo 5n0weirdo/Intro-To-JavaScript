bbd4f1cf78bebf195d04188fdca8f0a7fcd0f0a2
5n0weirdo <snoweirdo@protonmail.com>
2021-09-17 17:16:47 +0200

    Write JavaScript in a separate file

---
+++ 4-Writing JS in a separate file/01/index.js
@@ -0,0 +1,10 @@
+//A Method (prompt) that asks a user their name
+var userName = prompt("What is your name?");
+
+//Greets the user 
+    alert("Hello, " + userName);
+
+
+
+
+
---
+++ 4-Writing JS in a separate file/02/index.js
@@ -0,0 +1,5 @@
+//A Method that asks the user their favourite programming language 
+var devLanguage = prompt("What is your favourite programming language?");
+
+//Displays a sentence that contains the users' input
+    alert("You are a " + devLanguage + " " + "newbie, i guess.");
