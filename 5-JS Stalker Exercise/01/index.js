53ad8e724bb4dcc73e81e9875b03765a5873db52
5n0weirdo <snoweirdo@protonmail.com>
2021-09-18 18:56:37 +0200

    JavaScript Stalker Exercise

---
+++ 5-JS Stalker Exercise/01/index.js
@@ -0,0 +1,8 @@
+//Prompt Method is used to get input from the user:
+var userFirstname = prompt("What is your first name?");
+var userLastname = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+//the users' input is used and displayed back to them
+console.log("Your full name is" ,userFirstname ,userLastname);
+console.log("You are ", userAge ,"years old");
