e3de26612a36efb53f13d7fb421d061062043d98
5n0weirdo <snoweirdo@protonmail.com>
2021-09-18 19:04:08 +0200

    Days alive calculator

---
+++ 5-Age calculator Exercise/index.js
@@ -0,0 +1,6 @@
+var yearsLived = prompt("How old are you?");
+
+//calculating how many days the user has lived
+var caculatedDays = yearsLived * 365;
+
+alert(yearsLived + " " + "years is roughly" + " " + caculatedDays + " " + "days");
